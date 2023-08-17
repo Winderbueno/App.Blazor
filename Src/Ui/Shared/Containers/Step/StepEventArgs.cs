a5abaacd057e9fcaf91fec2bbdc4742f952a358a
winderbueno <kevin.gellenoncourt@gmail.com>
2023-08-17 20:54:34 +0200

    Cleaning

--- Src/Ui/Shared/Containers/Step/StepEventArgs.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ui.Shared.Containers.Step;
-
-public class StepEventArgs : EventArgs
-{
-    public StepElement Step { get; set; }
-
-    public StepEventArgs(StepElement step) => Step = step;    
-}
