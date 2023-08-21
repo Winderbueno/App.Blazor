9e77bfd150740c4740f45178dcfe84b637127728
winderbueno <kevin.gellenoncourt@gmail.com>
2023-08-21 11:15:06 +0200

    Cleaning

--- Src/Ui/Shared/Containers/Form/OptionHelper.cs
+++ Src/Ui/Shared/Containers/Form/OptionHelper.cs
@@ -5,25 +5,13 @@ namespace Ui.Shared.Containers.Form;
 
 public static class OptionHelper
 {
-    public static IEnumerable<Option> ToOption(this IEnumerable<string?> items)
-        => items.Select(item => new Option { 
-            Name = item, 
-            Value = item 
-        }).OrderBy(x => x.Name);
-
-    public static IEnumerable<Option> ToOption<TEnum>(this IEnumerable<TEnum> items)
-        => items.Select(item => new Option
-        {
-            Name = item.DisplayName(),
-            Value = item?.ToString()
-        }).OrderBy(x => x.Name);
-
-    public static IEnumerable<Option> ToOption<TObject>(this IEnumerable<TObject?> items,
-        Func<TObject?, string?> value,
-        Func<TObject?, string?> name)
-            => items.Select(item => new Option
+    // TData can be either a string, an Enum or an Object
+    public static IEnumerable<Option> ToOption<TData>(this IEnumerable<TData?> datas,
+        Func<TData?, string?>? name = null,
+        Func<TData?, string?>? value = null)
+            => datas.Select(data => new Option
             {
-                Name = name(item),
-                Value = value(item)
+                Name = name != null ? name(data) : data?.DisplayName(),
+                Value = value != null ? value(data) : data?.ToString()
             }).OrderBy(x => x.Name);
 }
