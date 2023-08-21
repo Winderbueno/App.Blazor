674f3607c5ebc3342fd2570a1ef75602d1e92f9c
winderbueno <kevin.gellenoncourt@gmail.com>
2023-08-21 16:01:11 +0200

    SelectField.Option.Refactor

--- Src/Ui/Shared/Containers/Form/Option.cs
+++ Src/Ui/Shared/Containers/Form/Option.cs
@@ -1,8 +1,26 @@
-﻿namespace Ui.Shared.Containers.Form;
+﻿using Domain.Extensions;
+
+namespace Ui.Shared.Containers.Form;
 
 public class Option
 {
     public string? Name { get; set; }
 
     public string? Value { get; set; }
+}
+
+public static class OptionExtensions
+{
+    // Convert a variable of any type in an Option
+    // TData can be either a string, an Enum or an Object
+    public static IEnumerable<Option> ToOption<TData>(this IEnumerable<TData?> datas,
+        Func<TData?, string?>? name = null,
+        Func<TData?, string?>? value = null)
+        => datas is IEnumerable<Option> options ?
+            options
+            : datas.Select(data => new Option
+            {
+                Name = name != null ? name(data) : data?.DisplayName(),
+                Value = value != null ? value(data) : data?.ToString()
+            }).OrderBy(x => x.Name);
 }
