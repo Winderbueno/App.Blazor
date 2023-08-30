3c8af90df21b5bf9e625b0c1a6975c691a1f05e8
winderbueno <kevin.gellenoncourt@gmail.com>
2023-08-30 13:08:37 +0200

    User.Create.Simplify

--- Src/Application/Validators/UserCreateValidator.cs
+++ Src/Application/Validators/UserCreateValidator.cs
@@ -7,8 +7,10 @@ public class UserCreateValidator : AbstractValidator<UserCreateAppDto>
 {
     public UserCreateValidator()
     {
-        RuleFor(x => x.Type).NotEmpty();
+        RuleFor(x => x.Type).NotNull();
         RuleFor(x => x.Function).NotEmpty();
+        RuleFor(x => x.Username).NotEmpty();
+
         RuleFor(x => x.FirstName).NotEmpty().MaximumLength(30);
         RuleFor(x => x.LastName).NotEmpty().MaximumLength(30);
         RuleFor(x => x.Email).NotEmpty().EmailAddress();
