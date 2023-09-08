f42c123f40f3807240646c355edaf6f91ecc1075
winderbueno <kevin.gellenoncourt@gmail.com>
2023-09-08 23:17:47 +0200

    User.Phone.Cleaning

--- Src/Application/Models/User/UserAppDto.cs
+++ Src/Application/Models/User/UserAppDto.cs
@@ -15,5 +15,4 @@ public class UserAppDto
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Email { get; set; }
-    public string? PhoneNumber { get; set; }
 }
