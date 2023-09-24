919acf9c45c7dbb0767f8befb1abd918f8873ed7
winderbueno <kevin.gellenoncourt@gmail.com>
2023-09-24 18:43:22 +0200

    ErrorHandling.Cleaning

--- Src/Infrastructure/HttpHandlers/ProblemDetailsHandler.cs
+++ Src/Infrastructure/HttpHandlers/ProblemDetailsHandler.cs
@@ -2,6 +2,7 @@
 using Infrastructure.HttpClients.Exceptions;
 using K.Blazor.Components.Indicators.Toast;
 using Newtonsoft.Json;
+using System.Net;
 
 namespace Infrastructure.HttpHandlers;
 
@@ -15,8 +16,10 @@ public class ProblemDetailsHandler : DelegatingHandler
     {
         var resp = await base.SendAsync(req, ct);
 
+        // Standard Handling for Api Business Errors
         var mediaType = resp.Content.Headers.ContentType?.MediaType;
-        if (!resp.IsSuccessStatusCode
+        if (HttpStatusCode.BadRequest.Equals(resp.StatusCode)
+            || HttpStatusCode.NotFound.Equals(resp.StatusCode)
             || (mediaType != null && mediaType.Equals("application/problem+json", StringComparison.InvariantCultureIgnoreCase)))
         {
             // For now, ProblemDetails is an homemade class (See class for more details)
