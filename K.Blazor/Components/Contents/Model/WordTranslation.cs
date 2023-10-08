e9f906ed6220bd948ce1fae0e227ff0e7acd6df8
winderbueno <kevin.gellenoncourt@gmail.com>
2023-10-08 20:34:10 +0200

    K.Blazor.Contents.AddWordTranslation

---
+++ K.Blazor/Components/Contents/Model/WordTranslation.cs
@@ -0,0 +1,8 @@
+﻿namespace K.Blazor.Components.Contents.Model;
+
+public class WordTranslation
+{
+    public string? English { get; set; }
+    public string? Pinyin { get; set; }
+    public string? FrenchPhonetics { get; set; }
+}
