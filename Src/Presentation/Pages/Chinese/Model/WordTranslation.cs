8f259e29b997a1f0d62242f83a0114dc42d9de78
winderbueno <kevin.gellenoncourt@gmail.com>
2023-11-02 14:17:53 +0100

    Chinese.Improve

--- Src/Presentation/Pages/Chinese/Model/WordTranslation.cs
+++ Src/Presentation/Pages/Chinese/Model/WordTranslation.cs
@@ -1,8 +1,8 @@
-﻿namespace K.Blazor.Components.Contents.Model;
+﻿namespace Presentation.Pages.Chinese.Model;
 
 public class WordTranslation
 {
     public string? English { get; set; }
     public string? Pinyin { get; set; }
-    public string? FrenchPhonetics { get; set; }
+    public string? Comment { get; set; }
 }
