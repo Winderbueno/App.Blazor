04a93c54d5be4d05101215a8e008a7f25573f856
Clean K <kevin.gellenoncourt@gmail.com>
2024-02-26 11:23:21 +0800

    Layout.Mobile.NavMenu.Display.Correction

--- Src/Presentation/wwwroot/scripts/scroll-observer.js
+++ Src/Presentation/wwwroot/scripts/scroll-observer.js
@@ -1,5 +1,9 @@
 function onScroll(elem, interval) {
     elem.addEventListener('scroll', function (e) {
+
+        // Does not activate on children element
+        if (elem !== e.target) return;
+
         // https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTop
         if (window.navMenuService != null)
             window.navMenuService.invokeMethodAsync('OnScroll', parseInt(e.target.scrollTop, 10));
